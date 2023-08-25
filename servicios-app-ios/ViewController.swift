785a683fc3da9699f16c10441bf952f58c30a3b2
Montse Medina <medinasevilam@gmail.com>
2023-08-25 09:11:04 -0400

    Initial Commit

---
+++ servicios-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  servicios-app-ios
+//
+//  Created by Montserrat Medina on 2023-08-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
